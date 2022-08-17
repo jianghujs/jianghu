11e7141183ad2043c24946da751078f004636d08
Liu Ji <mynameisliuji@foxmail.com>
2022-08-17 16:24:54 +0800

    app.js refactor

--- app.js
+++ app.js
@@ -3,21 +3,44 @@
 const { createJianghuKnex } = require('./app/common/jianghuKnexUtil');
 const socketIOInit = require('./app/controller/socketResource');
 
-// app.js
+/**
+ * 配置文件即将加载，这是最后动态修改配置的时机（configWillLoad）
+ * 配置文件加载完成（configDidLoad）
+ * 文件加载完成（didLoad）
+ * 插件启动完毕（willReady）
+ * worker 准备就绪（didReady）
+ * 应用启动完成（serverDidReady）
+ * 应用即将关闭（beforeClose）
+ */
 class AppBootHook {
   constructor(app) {
     this.app = app;
   }
 
-  async serverDidReady() {
+  configWillLoad() {
+  }
+
+  configDidLoad() {
+    // 挂载jianghuKnex
     this.app.jianghuKnex = createJianghuKnex(this.app.knex);
+  }
+
+  async didLoad() {
+  }
+
+  async willReady() {
     if (this.app.config.jiangHuConfig.enableSocket) {
       await socketIOInit(this.app);
     }
   }
 
-  // 所有配置已经加载完毕，用于自定义 Loader 挂载。
-  async didLoad() {
+  async didReady() {
+  }
+
+  async serverDidReady() {
+  }
+
+  async beforeClose() {
   }
 
 }
