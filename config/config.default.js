d2decf727f809a423063f208db9036e318b137c1
Liu Ji <mynameisliuji@foxmail.com>
2022-08-22 13:54:06 +0800

    config.default.js

--- config/config.default.js
+++ config/config.default.js
@@ -22,6 +22,7 @@ module.exports = appInfo => {
     indexPage: `/${appId}/page/manual`,
     loginPage: `/${appId}/page/login`,
     helpPage: `/${appId}/page/help`,
+    // TODO: { ...jiangHuConfig, ...jianghuConfig };
     jiangHuConfig: {
       enableSocket: false,
       packageIdCheck: false,
