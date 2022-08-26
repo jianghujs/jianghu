f3e052f547238bcdb8c169eab644be414424b265
illuz <iilluzen@gmail.com>
2022-08-26 21:30:02 +0800

    调整 diff 算法

--- app/common/diffUtil.js
+++ app/common/diffUtil.js
@@ -2,33 +2,44 @@
 // 用 id 做主 key，判断新旧两个 list 中的数据变化
 'use strict';
 
-function diff(oldList, newList) {
+function diff(oldList, newList, primaryKey = 'id') {
   const added = [];
   const removed = [];
   const changed = [];
 
-  const oldMap = Object.fromEntries(oldList.map(record => [ record.id, JSON.stringify(record) ]));
+  // 旧数据 primaryKey -> json
+  const oldMap = {};
+  oldList.forEach(record => {
+    const { id, ...compareBody } = record;
+    oldMap[record[primaryKey]] = JSON.stringify(compareBody);
+  });
+
   newList.forEach(record => {
-    if (!oldMap[record.id]) {
+    const primaryId = record[primaryKey];
+    if (!oldMap[primaryId]) {
       added.push(record);
       return;
     }
-    const oldJson = oldMap[record.id];
-    if (oldJson !== JSON.stringify(record)) {
-      // 这边可以做进一步字段对比
-      changed.push({ old: JSON.parse(oldJson), new: record });
+    const oldJson = oldMap[primaryId];
+    const { id, ...newCompareBody } = record;
+    if (oldJson !== JSON.stringify(newCompareBody)) {
+      const oldItem = oldList.find(o => o[primaryKey] === primaryId);
+      changed.push({ old: oldItem, new: record });
     }
     // 删除
-    delete oldMap[record.id];
+    delete oldMap[primaryId];
   });
 
   // 剩下的 oldMap 就是删除的
-  Object.entries(oldMap).forEach(([ id, recordJson ]) => {
-    removed.push(JSON.parse(recordJson));
+  Object.keys(oldMap).forEach(primaryId => {
+    const oldItem = oldList.find(o => String(o[primaryKey]) === String(primaryId));
+    removed.push(oldItem);
   });
 
   return { added, removed, changed };
 }
+
+// 使用场景1：对比时以 id 做主键 key
 
 // const res = diff(
 //   [
@@ -43,6 +54,25 @@ function diff(oldList, newList) {
 //     { id: 2, name: 'x' },
 //     { id: 7, name: 'e' },
 //   ]
+// );
+// console.log(JSON.stringify(res, null, '  '));
+
+// 使用场景2：对比时指定主键 key
+
+// const res = diff(
+//   [
+//     { id: 1, dataId: 'I', name: 'a' },
+//     { id: 2, dataId: 'II', name: 'b' },
+//     { id: 3, dataId: 'III', name: 'c' },
+//     { id: 4, dataId: 'IV', name: 'd' },
+//     { id: 5, dataId: 'V', name: 'e' },
+//   ],
+//   [
+//     { dataId: 'I', name: 'a' },
+//     { dataId: 'II', name: 'x' },
+//     { dataId: 'VII', name: 'e' },
+//   ],
+//   'dataId'
 // );
 // console.log(JSON.stringify(res, null, '  '));
 
