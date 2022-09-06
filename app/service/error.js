c07f7a22e0deba98b5c25a3a815fe8edb18ba1af
Liu Ji <mynameisliuji@foxmail.com>
2022-09-06 16:00:37 +0800

    3.0.59

---
+++ app/service/error.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// ========================================常用 require start===========================================
+const Service = require('egg').Service;
+const validateUtil = require('@jianghujs/jianghu/app/common/validateUtil');
+// ========================================常用 require end=============================================
+const actionDataScheme = Object.freeze({
+  htmlErrorLogRecord: {
+    type: 'object',
+    additionalProperties: true,
+    required: [ 'errorLogList' ],
+    properties: {
+      errorLogList: {
+        type: 'array',
+        items: {
+          type:'object',
+          additionalProperties: true,
+          required: [ 'errorTime', 'errorMessage' ],
+          properties: {
+            userId: { anyOf: [{ type: 'string' }, { type: 'number' }] },
+            deviceId: { anyOf: [{ type: 'string' }, { type: 'number' }] },
+            errorTime: { anyOf: [{ type: 'string' }] },
+            errorMessage: { anyOf: [{ type: 'string' }, { type: 'number' }] },
+          },
+        }
+      },
+    },
+  },
+});
+
+class HtmlErrorService extends Service {
+  async htmlErrorLogRecord() {
+   const htmlLogger = this.app.getLogger("htmlLogger");
+   const { actionData } = this.ctx.request.body.appData;
+   validateUtil.validate(actionDataScheme.htmlErrorLogRecord, actionData);
+   const { errorLogList } = actionData;
+   errorLogList.forEach(errorLog => {
+    htmlLogger.error("htmlErrorLogRecord", errorLog);
+   });
+  }
+}
+
+module.exports = HtmlErrorService;
