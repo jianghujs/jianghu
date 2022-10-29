3c569276d86727ea76eb633f3b816eb2b163b836
Liu Ji <mynameisliuji@foxmail.com>
2022-10-29 14:22:32 +0800

    onerror request_app_forbidden

--- config/config.default.js
+++ config/config.default.js
@@ -160,6 +160,7 @@ module.exports = appInfo => {
           errorCode === "request_token_invalid" ||
           errorCode === "request_user_not_exist" ||
           errorCode === "request_token_expired" ||
+          errorCode === "request_app_forbidden" ||
           errorCode === "user_banned"
         ) {
           ctx.cookies.set(`${ctx.app.config.appId}_authToken`, null);
