b8dbac4940e1a73b14f471c0109dc0e99540422f
illuz <iilluzen@gmail.com>
2023-03-01 00:39:04 +0800

    4.0.23: pageHook 支持使用 templateVar 配置字段的回写

--- app/middleware/pageHook.js
+++ app/middleware/pageHook.js
@@ -2,22 +2,22 @@
 
 module.exports = () => {
   return async (ctx, next) => {
-    const { logger } = ctx.app;
-
     ctx.hookResult = {};
     if (ctx.packagePage.pageHook) {
       const pageHook = JSON.parse(ctx.packagePage.pageHook);
       const beforeHookList = pageHook.beforeHook || [];
-      for (const { field, service, serviceFunc } of beforeHookList) {
+      for (const { field, templateVar, service, serviceFunc } of beforeHookList) {
         const result = await ctx.service[service][serviceFunc]();
         if (field) {
           ctx.hookResult[field] = result;
+        }
+        if (templateVar) {
+          ctx.hookResult[templateVar] = result;
         }
       }
     }
 
     await next();
-    // TODO: 记录 page 日志
   };
 };
 
