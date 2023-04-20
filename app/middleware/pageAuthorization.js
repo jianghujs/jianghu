895c714b1727217318aad03fa119f41fad9471e9
Liu Ji <mynameisliuji@foxmail.com>
2023-04-20 23:47:37 +0800

    pageAuthorization.js bugfix 4.0.36

--- app/middleware/pageAuthorization.js
+++ app/middleware/pageAuthorization.js
@@ -34,7 +34,7 @@ module.exports = option => {
     if (appType === "multiApp") {
       const targetUserApp =
         userAppList && userAppList.find((x) => x.appId === appId);
-      if (isNotAllow && !targetUserApp) {
+      if (!targetUserApp) {
         const { errorCode, errorReason } = errorInfoEnum.request_app_forbidden;
         ctx.redirect(
           `${ctx.app.config.loginPage}?errorCode=${errorCode}&errorReason=${errorReason}`
