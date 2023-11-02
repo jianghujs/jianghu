55cb236ad8fd30fae0e6774db576b1ce8a49e087
illuz <iilluzen@gmail.com>
2023-11-02 23:07:23 +0800

    去掉 _ui 表

--- app/controller/page.js
+++ app/controller/page.js
@@ -12,23 +12,10 @@ class PageController extends Controller {
   async page() {
     const { ctx } = this;
     const { packagePage, userInfo } = ctx;
-    const { jianghuKnex, logger } = this.app;
     const { userId, username, deviceId, deviceType } = userInfo.user || {};
-    const uiActionList = await jianghuKnex('_ui').whereIn('pageId', [ packagePage.pageId, 'allPage' ]).select();
-    // Tip: 为了避免 uiActionConfig json 异常, 这里 format一下
-    uiActionList.forEach(item => {
-      const { pageId, uiActionId, uiActionConfig} = item;
-      try {
-        item.uiActionConfig = JSON.parse(uiActionConfig || '{}');
-      } catch (err) {
-        item.uiActionConfig = { errorReason: 'uiActionConfig JSON.parse异常' }
-        logger.error('[page.js] _ui.uiActionConfig JSON.parse异常', { pageId, uiActionId, err })
-      }
-    });
     const targetHtml = packagePage.pageFile || `${packagePage.pageId}.html`;
     await ctx.render(`page/${targetHtml}`, {
       ...ctx.hookResult,
-      uiActionList,
       page: { passcode: packagePage.passcode }
     });
     this.app.getLogger('pageLogger').info({ pageId: packagePage.pageId, pageName: packagePage.pageName, userId, username, deviceId, deviceType });
