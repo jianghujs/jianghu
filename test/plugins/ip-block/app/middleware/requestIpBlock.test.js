d02da6b768af830ca07009f17d360048e316c1de
xiaomuxi <215597929@qq.com>
2023-11-06 21:38:23 +0800

    add UT to plugins ip-block

--- test/plugins/ip-block/app/middleware/requestIpBlock.test.js
+++ test/plugins/ip-block/app/middleware/requestIpBlock.test.js
@@ -5,6 +5,7 @@ const sinon = require('sinon');
 const path = require('path');
 const mock = require('egg-mock');
 const utils = require('../../../../utils');
+const fs = require('fs');
 const requestIpBlock = require('../../../../../plugins/ip-block/app/middleware/requestIpBlock');
 
 describe('test/app/middleware/requestIpBlock.test.js', () => {
@@ -18,30 +19,52 @@ describe('test/app/middleware/requestIpBlock.test.js', () => {
     this.app.close();
   });
 
-  describe('Test middleware requestIpBlock, ignored path/whitelist/blacklist', () => {
+  describe('Test middleware requestIpBlock', () => {
     beforeEach(() => {
       this.ctx = this.app.mockContext();
       this.ctx.app.config.jianghuConfig.enableRateLimiter = true;
       this.nextSpy = sinon.spy();
+      this.readFileSyncStub = sinon.stub(fs, 'readFileSync');
       this.requestIpBlock = requestIpBlock();
     });
     afterEach(() => {
       mock.restore();
+      this.readFileSyncStub.restore();
     });
 
-    it('IP whitelist, should success', async () => {
-      this.ctx.app.config.jianghuConfig.rateLimiterWhitelist = [ '127.0.0.1' ];
-      this.ctx.request.path = `/${this.app.config.appId}/notImportant/`;
+    it('enableIpBlock false, should success', async () => {
+      this.ctx.app.config.jianghuConfig.enableIpBlock = false;
       await this.requestIpBlock(this.ctx, this.nextSpy);
 
+      assert.equal(this.readFileSyncStub.callCount, 0);
       assert.equal(this.nextSpy.callCount, 1);
     });
 
-    it('IP blacklist, should success', async () => {
-      this.ctx.app.config.jianghuConfig.rateLimiterBlacklist = [ '127.0.0.1' ];
-      this.ctx.request.path = `/${this.app.config.appId}/notImportant/`;
-
+    it('ip not exist, should success', async () => {
+      this.ctx.app.config.jianghuConfig.enableIpBlock = true;
+      this.ctx.ip = null;
+      await this.requestIpBlock(this.ctx, this.nextSpy);
+      assert.equal(this.readFileSyncStub.callCount, 0);
+      assert.equal(this.nextSpy.callCount, 1);
+    });
+    it('in ip block list, should failed', async () => {
+      this.ctx.app.config.jianghuConfig.enableIpBlock = true;
+      this.ctx.app.config.jianghuConfig.ipBlocklist = [ '127.0.0.1' ];
+      this.ctx.ip = '127.0.0.1';
       assert.rejects(async () => await this.requestIpBlock(this.ctx, this.nextSpy), /request_rate_limit_exceeded/);
+      assert.equal(this.readFileSyncStub.callCount, 0);
+      assert.equal(this.nextSpy.callCount, 0);
+    });
+    it('in ipBlocklistFilePath, should failed', async () => {
+      this.ctx.app.config.jianghuConfig.enableIpBlock = true;
+      this.ctx.app.config.jianghuConfig.ipBlocklist = [ '127.0.0.1' ];
+      this.ctx.app.config.jianghuConfig.ipBlocklistFilePath = 'ipblocklistfile.txt';
+      this.ctx.ip = '127.0.0.2';
+      this.readFileSyncStub.returns('127.0.0.2\r\n127.0.0.1');
+      assert.rejects(async () => await this.requestIpBlock(this.ctx, this.nextSpy), /request_rate_limit_exceeded/);
+      assert.equal(this.readFileSyncStub.callCount, 1);
+      assert.equal(this.readFileSyncStub.getCall(0).args[0], 'ipblocklistfile.txt');
+      assert.equal(this.nextSpy.callCount, 0);
     });
 
   });
